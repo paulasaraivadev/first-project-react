57a14ac91deccf18a522c2c8d82a6f5f97d5551f
paulasaraivadev <engenharia-paulasaraiva@hotmail.com>
2024-03-06 13:38:02 -0300

    atualização 06.03.24

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
 import React from 'react';
-import './styles.css'
 
-const App = () => {
+import People from './Assets/people.svg';
 
-  const hello = "Hello React"
+import Arrow from './Assets/arrow.svg';
+
+import { 
+  Container, 
+  Image, 
+  ContainerItems, 
+  H1, 
+  InputLabel, 
+  Input, 
+  Button 
+} from './style';
+
+function App () {
+
+  const users = [
+    { id: Math.random(), name:"Rodolfo", age:28},
+    { id: Math.random(), name:"Maria", age:21},
+  ];
 
   return (
-    <div>
-      {hello}
-    </div>
+    <Container>
+      <Image alt="logo-image" src={People}/>
+      <ContainerItems>
+          
+        <H1>Olá!</H1>
+          
+        <InputLabel>Nome</InputLabel>
+        <Input placeholder="Nome"/>
+          
+        <InputLabel>Idade</InputLabel>
+        <Input placeholder="Idade"/>
+          
+        <Button>Cadastrar <img alt="seta" src={Arrow}/></Button>
+
+        <ul>
+          {users.map((user) =>(
+            <li key={user.id}>
+              {user.name} - {user.age}
+              </li>
+          ))}
+        </ul>
+        
+      </ContainerItems>
+    </Container>
   );
 }
 
-export default App
+export default App;
+
 
